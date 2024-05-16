2081cb2ade7a2635e20291c9677d96991811f7c1
Goutam-04 <goutamnayak2804+github1@gmail.com>
2024-05-16 12:01:12 +0530

    simple http server

--- server/index.js
+++ server/index.js
@@ -1,1 +1,16 @@
-a
+const express = require('express')
+const { createServer } = require('node:http');
+const { join } = require('node:path');
+const { Server } = require("socket.io");
+
+const app = express();
+const server = createServer(app);
+const io = new Server(server);
+
+const PORT= process.env.port||9000
+
+io.on('connection', (socket) => {
+    console.log('a user connected',socket.id);
+  });
+
+server.listen(PORT,()=>{console.log('app is running');})
