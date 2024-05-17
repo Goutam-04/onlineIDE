960774cd5d9476008a38c357fba8c2447deaf522
Goutam-04 <goutamnayak2804+github1@gmail.com>
2024-05-17 16:47:32 +0530

    backend of terminal in browser

--- server/index.js
+++ server/index.js
@@ -1,34 +1,40 @@
-const express = require('express')
-const { createServer } = require('node:http');
-const { join } = require('node:path');
+const express = require("express");
+const { createServer } = require("node:http");
+const { join } = require("node:path");
 const { Server } = require("socket.io");
 
-var pty = require('node-pty-prebuilt-multiarch');
-var ptyProcess = pty.spawn(bash, [], {
-    name: 'xterm-color',
-    cols: 80,
-    rows: 30,
-    cwd: process.env.INIT_CWD,
-    env: process.env
-  });
+var os = require("os");
+var pty = require("node-pty");
+
+var shell = os.platform() === "win32" ? "powershell.exe" : "bash";
+
+var ptyProcess = pty.spawn(shell, [], {
+  name: "xterm-color",
+  cols: 80,
+  rows: 30,
+  cwd: process.env.INIT_CWD,
+  env: process.env,
+});
 
 const app = express();
 const server = createServer(app);
 const io = new Server(server);
 
-const PORT= process.env.port||9000
+const PORT = 9000;
 
-ptyProcess.onData(data => {
-    io.emit('terminal:data', data)
-})
+ptyProcess.onData((data) => {
+  console.log(data);
+  io.emit("terminal:data", data);
+});
 
-
-io.on('connection', (socket) => {
-    console.log('a user connected',socket.id);
-    socket.on('terminal:write', (data) => {
-        console.log('Term', data)
-        ptyProcess.write(data);
-    })
+io.on("connection", (socket) => {
+  console.log("a user connected", socket.id);
+  socket.on("terminal:write", (data) => {
+    console.log("Term", data);
+    ptyProcess.write(data);
   });
+});
 
-server.listen(PORT,()=>{console.log('app is running');})
+server.listen(PORT, () => {
+  console.log("app is running");
+});
